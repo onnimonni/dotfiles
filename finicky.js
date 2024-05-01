cafd4be46a6e6be090511b134ab877333249186a
Onni Hakala <onni@flaky.build>
2024-05-01 12:45:06 +0200

    Cleanup finicky file

--- finicky.js
+++ finicky.js
@@ -1,62 +1,59 @@
-// See more https://github.com/pablouser1/ProxiTok/wiki/Public-instances
-const TIKTOK_PROXIES = [
-  "proxitok.pabloferreiro.es", // Official
-  "proxitok.pussthecat.org",
-  "tok.habedieeh.re",
-  "proxitok.esmailelbob.xyz",
-  "proxitok.privacydev.net",
-  "tok.artemislena.eu",
-  "tok.adminforge.de",
-  "tt.vern.cc",
-  "cringe.whatever.social",
-  "proxitok.lunar.icu",
-  "proxitok.privacy.com.de",
-  "cringe.seitan-ayoub.lol",
-  "cringe.datura.network",
-  "tt.opnxng.com",
-  "proxitok.tinfoil-hat.net",
-  "tiktok.wpme.pl",
-  "proxitok.r4fo.com",
-  "proxitok.belloworld.it",
-]
-// Function to select random TikTok proxy
-const selectRandomTikTokProxy = () => {
-  return TIKTOK_PROXIES[Math.floor(Math.random() * TIKTOK_PROXIES.length)]
-}
-
 module.exports = {
-    defaultBrowser: "Safari",
-    rewrite: [
-      {
-        // Redirect all urls to use https except localhost
-        match: ({ url }) => url.protocol === "http" && !finicky.matchHostnames(['localhost']),
-        url: { protocol: "https" }
-      },
-      {
-        // Redirect Tiktok video links to use Proxitok public proxies
-        match: ({ url }) => (url.host.endsWith("tiktok.com") && url.pathname.startsWith('/@')) || url.host.endsWith("vm.tiktok.com"),
-        url: ({ url }) => {
-          return {
-            protocol: "https",
-            host: selectRandomTikTokProxy(),
-            // Prepend pathname with /@placeholder/video to match ProkiTok urls
-            pathname: (url.pathname.startsWith('/@') ? url.pathname : `/@placeholder/video${url.pathname}`)
-          }
+  defaultBrowser: "Safari",
+  rewrite: [
+    {
+      // Redirect all urls to use https except localhost
+      match: ({ url }) => url.protocol === "http" && !finicky.matchHostnames(['localhost']),
+      url: { protocol: "https" }
+    },
+    {
+      // Redirect Tiktok video links to use Proxitok public proxies
+      match: ({ url }) => (url.host.endsWith("tiktok.com") && url.pathname.startsWith('/@')) || url.host.endsWith("vm.tiktok.com"),
+      url: ({ url }) => {
+        // See more https://github.com/pablouser1/ProxiTok/wiki/Public-instances
+        const selectRandomTikTokProxy = () => {
+          const TIKTOK_PROXIES = [
+            "proxitok.pabloferreiro.es", // Official
+            "proxitok.pussthecat.org",
+            "tok.habedieeh.re",
+            "proxitok.esmailelbob.xyz",
+            "proxitok.privacydev.net",
+            "tok.artemislena.eu",
+            "tok.adminforge.de",
+            "tt.vern.cc",
+            "cringe.whatever.social",
+            "proxitok.lunar.icu",
+            "proxitok.privacy.com.de",
+            "cringe.seitan-ayoub.lol",
+            "cringe.datura.network",
+            "tt.opnxng.com",
+            "tiktok.wpme.pl",
+            "proxitok.r4fo.com",
+            "proxitok.belloworld.it",
+          ]
+          return TIKTOK_PROXIES[Math.floor(Math.random() * TIKTOK_PROXIES.length)]
+        }
+        return {
+          protocol: "https",
+          host: selectRandomTikTokProxy(),
+          // Prepend pathname with /@placeholder/video to match ProkiTok urls
+          pathname: (url.pathname.startsWith('/@') ? url.pathname : `/@placeholder/video${url.pathname}`)
         }
       }
-    ],
-    handlers: [
-      {
-        // Open google.com and *.google.com urls in Google Chrome
-        match: [
-          "google.com/*", // match google.com urls
-          "*.google.com/*", // match google.com subdomains
-          "*.cloud.google.com/*", // Use GCP with chrome
-          "github.com/*",
-          "*.app.netsuite.com/*",
-          "swappie.*" // Login to Swappie systems with Chrome
-        ],
-        browser: "Google Chrome"
-      }
-    ]
-  };
+    }
+  ],
+  handlers: [
+    {
+      // Open google.com and *.google.com urls in Google Chrome
+      match: [
+        "google.com/*", // match google.com urls
+        "*.google.com/*", // match google.com subdomains
+        "*.cloud.google.com/*", // Use GCP with chrome
+        "github.com/*",
+        "*.app.netsuite.com/*",
+        "swappie.*" // Login to Swappie systems with Chrome
+      ],
+      browser: "Google Chrome"
+    }
+  ]
+};
