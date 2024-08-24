20a5d79ff7f3a6a865f322f4abc61cf2ae9e265f
Onni Hakala <onni@flaky.build>
2024-08-24 10:53:26 +0300

    Use bat instead of pygmentize for coloring outputs in terminal

--- config/fish/functions/c.fish
+++ config/fish/functions/c.fish
@@ -1,8 +1,8 @@
 # `cat` with beautiful colors. uses Pygments if installed.
 # Otherwise fallbacks to basic cat
 function c --description 'Print file contents with colors'
-  if command_exists pygmentize
-    pygmentize -O style=monokai -f console256 -g $argv
+  if command_exists bat
+    bat $argv
   else
     cat --color=auto $argv
   end
