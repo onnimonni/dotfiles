b1e9553e66aa766cf11fab5bb7e7b04c8db0174d
Onni Hakala <onni@flaky.build>
2025-01-13 18:56:27 +0100

    Simplify tiktok proxy setup

--- finicky.js
+++ finicky.js
@@ -7,44 +7,15 @@ module.exports = {
       url: { protocol: "https" }
     },
     {
-      // Redirect Tiktok video links to use Proxitok public proxies
+      // TODO: This gets links like: https://vm.tiktok.com/ZNeKFD54M
+      // Redirect them to: https://vm.offtiktok.com/ZNeKFD54M instead
+      // Redirect Tiktok video links to use offtiktok.com
       match: ({ url }) => url.host.endsWith("tiktok.com"),
       url: ({ url }) => {
-        // See more https://github.com/pablouser1/ProxiTok/wiki/Public-instances
-        const selectRandomTikTokProxy = () => {
-          const TIKTOK_PROXIES = [
-            "proxitok.pabloferreiro.es", // Official
-            "proxitok.pussthecat.org",
-            "tok.habedieeh.re",
-            "proxitok.esmailelbob.xyz",
-            "proxitok.privacydev.net",
-            "tok.artemislena.eu",
-            "tok.adminforge.de",
-            "tt.vern.cc",
-            "cringe.whatever.social",
-            "proxitok.lunar.icu",
-            "proxitok.privacy.com.de",
-            "cringe.seitan-ayoub.lol",
-            "cringe.datura.network",
-            "tt.opnxng.com",
-            "tiktok.wpme.pl",
-            "proxitok.r4fo.com",
-            "proxitok.belloworld.it",
-          ]
-          return TIKTOK_PROXIES[Math.floor(Math.random() * TIKTOK_PROXIES.length)]
-        }
-        const tikTokUrlToProxitok = (url) => {
-          if (url.pathname.startsWith('/@'))
-            return url.pathname
-          if (url.pathname.startsWith('/t/'))
-            return url.pathname.replace('/t/', "/@placeholder/video/")
-          return `/@placeholder/video${url.pathname}`
-        }
         return {
           protocol: "https",
-          host: selectRandomTikTokProxy(),
-          // Prepend pathname with /@placeholder/video to match ProkiTok urls
-          pathname: tikTokUrlToProxitok(url)
+          host: "vm.offtiktok.com",
+          pathname: url.pathname
         }
       }
     }
