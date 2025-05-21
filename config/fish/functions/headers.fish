947f0e91709f0dfe1df1fae6e1f9dfe18f0a2ff4
Onni Hakala <onni@flaky.build>
2025-05-21 17:53:45 +0200

    Document fake user agent in headers helper

--- config/fish/functions/headers.fish
+++ config/fish/functions/headers.fish
@@ -2,6 +2,11 @@
 # It also follows redirects
 # Usage: $ headers google.fi
 function headers --description 'Print all http headers of http GET request'
+  # Some stupid services have WAF based on the User-Agent header
+  # Discovering what's going on is harder without a proper User-Agent
+  set -l FAKE_USER_AGENT 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36'
+
+  # -IXGET throws away the body like HEAD does and uses GET http verb
   # Source: https://www.woolie.co.uk/article/curl-full-get-request-dropping-body/
-  curl -L -IXGET -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36' $argv
+  curl -L -IXGET -H "User-Agent: $FAKE_USER_AGENT" $argv
 end
