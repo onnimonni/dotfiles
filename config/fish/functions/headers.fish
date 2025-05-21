46e5b0aea6d369570637ef0007d7b647cdfedd2d
Onni Hakala <onni@flaky.build>
2025-05-21 17:49:06 +0200

    Add fake user agent and don't wait for body when asking for HTTP headers

--- config/fish/functions/headers.fish
+++ config/fish/functions/headers.fish
@@ -1,6 +1,7 @@
 # This is normal GET request instead of HEAD request
 # It also follows redirects
 # Usage: $ headers google.fi
-function headers --description 'Print all headers of request'
-    curl -LsD - $argv -o /dev/null
+function headers --description 'Print all http headers of http GET request'
+  # Source: https://www.woolie.co.uk/article/curl-full-get-request-dropping-body/
+  curl -L -IXGET -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36' $argv
 end
