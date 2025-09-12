6a967a6d3822fb6272409c458db7ea34c6f40ac5
Onni Hakala <onni@flaky.build>
2025-09-12 14:08:00 +0300

    Fix the git config because on nix-darwin one needs to use home-manager for git settings

--- darwin/system/programs/git.nix
+++ darwin/system/programs/git.nix
@@ -1,54 +1,53 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
-  programs.git = {
-    enable = true;
+  home-manager.users.${config.system.primaryUser} = { pkgs, ... }: {
+    programs.git = {
+      enable = true;
 
-    attributes = [ "*.lockb binary diff=lockb" ];
+      attributes = [ "*.lockb binary diff=lockb" ];
 
-    extraConfig = {
-      include.path = "~/.gituser";
-      commit.gpgsign = true;
+      extraConfig = {
+        commit.gpgsign = true;
 
-      diff.lockb = {
-        textconv = "bun";
-        binary = true;
+        diff.lockb = {
+          textconv = "bun";
+          binary = true;
+        };
+
+        core = {
+          editor = "nvim";
+          autocrlf = false;
+          quotePath = false;
+        };
+
+        push.default = "simple";
+        pull.rebase = true;
+        fetch.prune = true;
+        branch.autosetuprebase = "always";
+        init.defaultBranch = "main";
+        rerere.enabled = true;
+        color.ui = true;
+
+        blame.date = "relative";
+
+        "color \"diff-highlight\"" = {
+          oldNormal = "red bold";
+          oldHighlight = "red bold";
+          newNormal = "green bold";
+          newHighlight = "green bold ul";
+        };
+
+        "color \"diff\"" = {
+          meta = "yellow";
+          frag = "magenta bold";
+          commit = "yellow bold";
+          old = "red bold";
+          new = "green bold";
+          whitespace = "red reverse";
+        };
       };
 
-      core = {
-        editor = "nvim";
-        autocrlf = false;
-        quotePath = false;
-      };
-
-      push.default = "simple";
-      pull.rebase = true;
-      fetch.prune = true;
-      branch.autosetuprebase = "always";
-      init.defaultBranch = "master";
-      rerere.enabled = true;
-      color.ui = true;
-
-      blame = {
-        date = "relative";
-      };
-
-      "color \"diff-highlight\"" = {
-        oldNormal = "red bold";
-        oldHighlight = "red bold";
-        newNormal = "green bold";
-        newHighlight = "green bold ul";
-      };
-
-      "color \"diff\"" = {
-        meta = "yellow";
-        frag = "magenta bold";
-        commit = "yellow bold";
-        old = "red bold";
-        new = "green bold";
-        whitespace = "red reverse";
-      };
+      delta.enable = true;
     };
-
-    diff-so-fancy.enable = true;
   };
 }
