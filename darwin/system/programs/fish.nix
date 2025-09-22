9f617b94ba3211428837aa9c58f28f1c1fcbd70a
Onni Hakala <onni@flaky.build>
2025-09-22 08:52:25 +0300

    Disable interactive session bash to fish because it's broken

--- darwin/system/programs/fish.nix
+++ darwin/system/programs/fish.nix
@@ -3,16 +3,18 @@
   users.users.onnimonni.shell = pkgs.fish;
   home-manager.users.onnimonni.programs.fish.enable = true;
 
+  # FIXME: Fails often like this:
+  # /nix/store/5kyj36g08zq4xi5311fww00b39jcb0bg-procps-1003.1-2008/bin/ps: illegal option -- -
   # Force fish for interactive sessions
-  programs.bash = {
-    interactiveShellInit = ''
-      if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-      then
-        shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-        exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-      fi
-    '';
-  };
+  #programs.bash = {
+  #  interactiveShellInit = ''
+  #    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+  #    then
+  #      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+  #      exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+  #    fi
+  #  '';
+  #};
 
   # This disables annoying message when opening a new terminal:
   # Last login: Sun May 25 21:52:09 on ttys009
@@ -28,6 +30,12 @@
     shellAliases = {
       # Update this config
       update-nix = "sudo darwin-rebuild switch --flake ~/.dotfiles/";
+
+      update-all = ''
+        nix flake update --flake ~/.dotfiles && \
+        sudo darwin-rebuild switch --flake ~/.dotfiles/ && \
+        duckdb -c "UPDATE EXTENSIONS;"
+      '';
 
       # Reload fish config
       reload-fish = "source ~/.config/fish/config.fish";
