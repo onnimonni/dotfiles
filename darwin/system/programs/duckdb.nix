d294f37d9cf04a05dd4cbb26686937db5784be06
Onni Hakala <onni@flaky.build>
2025-10-20 09:58:53 +0300

    Install duckdb from homebrew because nixpkgs is lacking 1.4.X

--- darwin/system/programs/duckdb.nix
+++ darwin/system/programs/duckdb.nix
@@ -1,32 +1,22 @@
 { pkgs, ... }:
-let
-  latestDuckDB = pkgs.duckdb.version;
-  # As of 2024-09, latest version is 1.4.0 but it's not yet merged for nixpkgs
-  # We are overriding even that and using unreleased version instead
-  expectedVersion = "1.4.0";
-  customDuckDB = pkgs.duckdb.overrideAttrs (oldAttrs: rec {
-    version = "1.4.0";
-    rev = "b8a06e4a22672e254cd0baa68a3dbed2eb51c56e";
-    src = pkgs.fetchFromGitHub {
-      owner = "duckdb";
-      repo = "duckdb";
-      tag = "v${version}";
-      sha256 = "sha256-ywQU+G8+VF+CiCb0Kgnx9cqKBBUEs4JG0iqh/OQS980=";
-    };
-  });
-in
 {
-  environment.systemPackages = [
-    # DuckDB for data mangling
-    pkgs.duckdb
-    #(if (latestDuckDB > expectedVersion)
-    #  then (throw "DuckDB ${latestDuckDB} is newer than ${expectedVersion}. Custom override can be removed.")
-    #  else customDuckDB)
+  homebrew.brews = [
+    # Homebrew has currently newer duckdb than nix
+    # See more in: https://github.com/NixOS/nixpkgs/pull/444225
+    (
+      if (pkgs.duckdb.version > "1.4.0")
+      then (throw "DuckDB ${pkgs.duckdb.version} is newer than 1.4.0. You can install duckdb again from nixpkgs.")
+      else "duckdb"
+    )
   ];
 
   home-manager.users.onnimonni.home.file = {
     # Automatically load few duckdb extensions
     ".duckdbrc".text = ''
+      INSTALL netquack FROM community;
+      LOAD netquack;
+      INSTALL dns FROM community;
+      LOAD dns;
       INSTALL spatial;
       LOAD spatial;
       INSTALL httpfs;
