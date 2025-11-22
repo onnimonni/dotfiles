3fce7dc76f6b3b14c0e6682b061ea0e5b0328098
Onni Hakala <onni@flaky.build>
2025-11-22 12:35:34 +0200

    Add initial rewrite of ~/.ssh/config

---
+++ darwin/system/programs/ssh.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }:
+{
+  # Configure SSH to include secret_config
+  home-manager.users.onnimonni.programs.ssh = {
+    enable = true;
+    includes = [
+      "~/.colima/ssh_config"
+      "~/.ssh/secret_config"
+    ];
+
+    # Use ssh keys through secretive
+    matchBlocks."*" = {
+      identityAgent = "/Users/onnimonni/Library/Containers/com.maxgoedjen.Secretive.SecretAgent/Data/socket.ssh";
+    };
+  };
+}
