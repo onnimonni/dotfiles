118cce57e4e22ee4d4d4c8485b1fec19fe2978cb
Onni Hakala <onni@flaky.build>
2026-02-11 20:25:22 +0200

    Use nix-darwin fork with mas GUI session fix

--- flake.nix
+++ flake.nix
@@ -9,7 +9,9 @@
     #determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/0.1";
 
     nix-darwin = {
-      url = "github:lnl7/nix-darwin";
+      # Use fork with fix for mas App Store auth in user's GUI session
+      # PR: https://github.com/nix-darwin/nix-darwin/issues/1694
+      url = "github:onnimonni/nix-darwin/fix/mas-user-session";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
